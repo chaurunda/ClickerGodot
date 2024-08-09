a28537d1b9c99ce838440360b3c593f304ed6d67
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-09 16:25:59 +0200

    refactor: use snake_case for Menu

--- components/Menu/menu.gd
+++ components/Menu/menu.gd
@@ -1,18 +1,18 @@
 extends Control
 
-@onready var loadButton = %LoadButton
-@onready var saveButton = %SaveButton
-@onready var newGameButton = %NewGameButton
-var isGameLaunched = false
+@onready var load_button = %LoadButton
+@onready var save_button = %SaveButton
+@onready var new_game_button = %NewGameButton
+var is_game_launched = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if SaveManager.has_save():
-		loadButton.disabled = false
+		load_button.disabled = false
 
 func _on_new_game_button_pressed():
 	self.visible = false
-	isGameLaunched = true
+	is_game_launched = true
 	DbManager.new_game()
 
 func _on_save_button_pressed():
@@ -21,16 +21,16 @@ func _on_save_button_pressed():
 func _on_load_button_pressed():
 	SaveManager.load()
 	self.visible = false
-	isGameLaunched = true
+	is_game_launched = true
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 
 func _process(_delta):
-	if isGameLaunched:
-		saveButton.visible = true
-		loadButton.visible = false
-		newGameButton.visible = false
+	if is_game_launched:
+		save_button.visible = true
+		load_button.visible = false
+		new_game_button.visible = false
 
 	if Input.is_action_pressed("escape"):
 		self.visible = true
