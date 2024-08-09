03bf13737d5d7119eb02e25e3740818fcda07ab2
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-09 16:24:44 +0200

    refactor: use snake_case for Inn

--- components/Inn/Inn.gd
+++ components/Inn/Inn.gd
@@ -1,64 +1,64 @@
 extends Area2D
 
 @onready var timer = $Timer
-@onready var timerUpgradeButton = $UpgradeUI/TimerUpgradeButton
-@onready var amountUpgradeButton = $UpgradeUI/AmountUpgradeButton
-@onready var upgradeUI = $UpgradeUI
+@onready var timer_upgrade_button = $UpgradeUI/TimerUpgradeButton
+@onready var amount_upgrade_button = $UpgradeUI/AmountUpgradeButton
+@onready var upgrade_ui = $UpgradeUI
 
-@export var baseAmount = 10
+@export var base_amount = 10
 
 
-var timerInnBuilding = Building.new("InnTimer", 0)
-var amountInnBuilding = Building.new("InnAmout", 1)
+var timer_inn_building = Building.new("InnTimer", 0)
+var amount_inn_building = Building.new("InnAmout", 1)
 
-const arrayOfWaitingTimes = [10, 6, 2, 1, .5, .1]
-var timerUpgradeCount = timerInnBuilding.get_cost_per_level()
+const array_of_waiting_times = [10, 6, 2, 1, .5, .1]
+var timer_upgrade_count = timer_inn_building.get_cost_per_level()
 
-var amountOfGold = 0
-var amountUpgradeCount = amountInnBuilding.get_cost_per_level()
+var amount_of_gold = 0
+var amount_upgrade_count = amount_inn_building.get_cost_per_level()
 
 func _ready():
-	setTimerButtonLabel()
-	setAmountButtonLabel()
-	setTimer()
-	upgradeUI.visible = false
+	set_timer_button_label()
+	set_amount_button_label()
+	set_timer()
+	upgrade_ui.visible = false
 
 func _on_timer_timeout():
-	timerInnBuilding.increase_money(amountOfGold)
+	timer_inn_building.increase_money(amount_of_gold)
 
 func _on_timer_upgrade_button_pressed():
-	var hasUpgrade = timerInnBuilding.upgrade_building()
-	if hasUpgrade:
-		timerUpgradeCount += timerUpgradeCount
-		setTimer()
-		setTimerButtonLabel()
+	var has_upgrade = timer_inn_building.upgrade_building()
+	if has_upgrade:
+		timer_upgrade_count += timer_upgrade_count
+		set_timer()
+		set_timer_button_label()
 
 func _on_amount_upgrade_button_pressed():
-	var hasUpgrade = amountInnBuilding.upgrade_building()
-	if hasUpgrade:
-		amountUpgradeCount += amountUpgradeCount
-		setAmountButtonLabel()
+	var has_upgrade = amount_inn_building.upgrade_building()
+	if has_upgrade:
+		amount_upgrade_count += amount_upgrade_count
+		set_amount_button_label()
 
-func setTimer():
-	timer.wait_time = arrayOfWaitingTimes[timerInnBuilding.building_level]
+func set_timer():
+	timer.wait_time = array_of_waiting_times[timer_inn_building.building_level]
 
 func _on_close_button_pressed():
-	upgradeUI.visible = false
+	upgrade_ui.visible = false
 
 func _on_upgrade_button_pressed():
-	upgradeUI.visible = true
+	upgrade_ui.visible = true
 
-func setTimerButtonLabel():
-	timerUpgradeButton.text = tr("UPGRADE_WITH_COST") % [timerInnBuilding.building_level + 1, timerInnBuilding.get_cost_per_level()]
+func set_timer_button_label():
+	timer_upgrade_button.text = tr("UPGRADE_WITH_COST") % [timer_inn_building.building_level + 1, timer_inn_building.get_cost_per_level()]
 
-func setAmountButtonLabel():
-	amountUpgradeButton.text = tr("UPGRADE_WITH_COST") % [amountInnBuilding.building_level, amountInnBuilding.get_cost_per_level()]
+func set_amount_button_label():
+	amount_upgrade_button.text = tr("UPGRADE_WITH_COST") % [amount_inn_building.building_level, amount_inn_building.get_cost_per_level()]
 
 func save():
-	var saveData = {
-		"building_level": timerInnBuilding.building_level,
-		"amountInnBuildingLevel": amountInnBuilding.building_level,
+	var save_data = {
+		"building_level": timer_inn_building.building_level,
+		"amountInnBuildingLevel": amount_inn_building.building_level,
 		"filename" : get_scene_file_path(),
 		"parent" : get_parent().get_path(),
 	}
-	return saveData
+	return save_data
