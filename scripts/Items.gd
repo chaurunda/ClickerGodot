9ea12379ae13ff16952e6d7b9b43aa14c72bd00f
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-09 16:06:28 +0200

    Refactor variable names in Items class to use snake_case convention

--- scripts/Items.gd
+++ scripts/Items.gd
@@ -1,6 +1,6 @@
 class_name Items
 
-var listOfWoodenItems: Array[Item]= [
+var list_of_wooden_items: Array[Item]= [
   Item.new('Wooden Sword', 1, Stats.new(0, 10, 0), Item.ITEMS.WEAPON, 1),
   Item.new('Wooden Helmet', 1, Stats.new(1, 0, 1), Item.ITEMS.HELMET, 2),
   Item.new('Wooden Legs', 1, Stats.new(1, 0, 1), Item.ITEMS.LEGS, 3),
