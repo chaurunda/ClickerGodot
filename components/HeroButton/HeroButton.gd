754378de7df6f6bcf43014de2ffd4f5acc44d3ec
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-09 16:12:02 +0200

    refactor: use snake_case for HeroButton

--- components/HeroButton/HeroButton.gd
+++ components/HeroButton/HeroButton.gd
@@ -2,8 +2,8 @@ extends Button
 
 class_name HeroButton
 
-var currentHeroId: HeroManager.HEROID
+var current_hero_id: HeroManager.HEROID
 
 func _on_pressed():
-	HeroManager.set_current_hero_selected(currentHeroId)
+	HeroManager.set_current_hero_selected(current_hero_id)
 	GlobalEventBus.hero_selected.emit()
