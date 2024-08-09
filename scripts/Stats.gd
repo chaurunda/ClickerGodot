70598c4eab6e8bf23d10d0dea95ee1d7cbd5c792
Huc Arnaud <arnaud.huc@adevinta.com>
2024-08-09 16:07:50 +0200

    refactor: use snake_case for Stats

--- scripts/Stats.gd
+++ scripts/Stats.gd
@@ -4,7 +4,7 @@ var health: int = 0
 var attack: int = 0
 var armor: int = 0
 
-func _init(healthStats, attackStats, armorStats):
-  health = healthStats
-  attack = attackStats
-  armor = armorStats
+func _init(health_stats, attack_stats, armor_stats):
+  health = health_stats
+  attack = attack_stats
+  armor = armor_stats
